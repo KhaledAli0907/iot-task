78517807be14fa115c059fc30ef558573cb4861a
khaledali0907 <khaledali0907@gmail.com>
2026-02-06 15:53:53 +0200

    fix: update api url

--- wokwi/src/config.h
+++ wokwi/src/config.h
@@ -6,7 +6,7 @@
 #define WIFI_PASS ""
 
 // Backend URL (must include /readings for POST)
-#define API_URL "https://von-medicinable-clannishly.ngrok-free.dev/readings"
+#define API_URL "https://iot-task-production.up.railway.app/readings"
 
 // TIMING
 #define SEND_INTERVAL 5000
